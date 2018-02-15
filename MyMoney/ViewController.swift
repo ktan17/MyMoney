a5e3b9f6366658f273fa8a9f9fac238017b9a83d
ktan17 <Kevin.Tan@gpiglobal.com>
2018-02-14 23:20:10 -0800

    Initial Commit

---
+++ MyMoney/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMoney
+//
+//  Created by Kevin Tan on 2/14/18.
+//  Copyright © 2018 ACM Hack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
